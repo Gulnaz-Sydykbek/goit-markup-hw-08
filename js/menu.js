4187e3d3033b510136534ce358a7af96a5719704
Gulnaz-Sydykbek <gulnaz.sydykbekova.94@mail.ru>
2021-02-15 09:06:04 +0600

    ДЗ №8

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+/*(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobilMenuRef = document.querySelector("[data-menu]");
+
+    menuBtnRef.addEventListener("click", () => {
+        const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobilMenuRef.classList.toggle("is-open");
+    });
+})();*/
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-btn]'),
+    closeModalBtn: document.querySelector('[data-modal-close-btn]'),
+    modal: document.querySelector('[data-modal-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('is-hidden-btn');
+  }
+})();
